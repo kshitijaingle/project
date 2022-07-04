f1cee79f2bf91c42f1cc3eaf3c3c94a777d2116b
kshitijaingle97@gmail.com <kshitijaingle97@gmail.com>
2022-07-04 14:42:01 +0530

    added bubble file

---
+++ first.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void bubblesort(int arr[],int n)
+{   int i,j,temp;
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    
+}
+int main()
+{
+    int arr[5]={5,8,6,4,3};
+    int i;
+    int n=sizeof(arr)/sizeof(arr[0]);
+    bubblesort(arr,n);
+    for(i=0;i<n;i++)
+    printf("%d\n",arr[i]);
+}
